5856dea4353e7e49ecda03e64d265dba64c750ce
syp <focograal@gmail.com>
2023-03-21 21:54:10 -0300

    painel

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  res.sendFile('index.html', { root: __dirname })
+});
+
+
+
+const porta = process.env.PORT || 5000;
+//iniciando...
+app.listen(porta, () => console.log("site Online na porta:", porta));
